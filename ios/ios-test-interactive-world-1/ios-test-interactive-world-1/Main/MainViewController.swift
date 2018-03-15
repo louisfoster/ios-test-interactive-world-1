0bd37c870eca2e6374d831198e38c007256acdb2
Louis Foster <123@louisfoster.com>
2018-03-15 22:15:11 +1100

    setup project

---
+++ ios/ios-test-interactive-world-1/ios-test-interactive-world-1/Main/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  ios-test-interactive-world-1
+//
+//  Created by Louie on 15/3/18.
+//  Copyright © 2018 Louis Foster. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet
+    private var sceneView: SCNView?
+}
